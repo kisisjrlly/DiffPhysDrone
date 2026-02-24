6bc7066a1e66f3b940f622769afe7535f428e4e7
zhaoguodong <1690253916@qq.com>
2026-02-24 18:26:03 +0800

    Add detailed Chinese comments to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# 运行脚本 (Run script)
+
+# 设置要运行的任务名称 (Set the task to run)
 task=paper_gdac
 
-
+# 获取当前日期和时间，用于日志文件名 (Get current date and time for log file name)
 date=$(date +%Y-%m-%d-%H-%M-%S)
 
+# 运行主程序，读取对应的配置文件，并将输出重定向到日志文件
+# (Run the main program, read the corresponding config file, and redirect output to a log file)
 python -u main_cuda.py $(cat configs/$task.args) > logs/$task-$date.log 2>&1
