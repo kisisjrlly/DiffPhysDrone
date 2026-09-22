5813cdac3b49b0bc4774d01bf40bfe20a682e025
Guodong Zhao <34215214+kisisjrlly@users.noreply.github.com>
2026-09-22 10:10:36 +0800

    feat: export grayscale camera builder

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,4 +1,4 @@
 from .gray_camera_semantics import GrayCameraSemantics
-from .differentiable_gray_camera import DifferentiableGrayCamera
+from .differentiable_gray_camera import DifferentiableGrayCamera, build_from_args
 
-__all__ = ["GrayCameraSemantics", "DifferentiableGrayCamera"]
+__all__ = ["GrayCameraSemantics", "DifferentiableGrayCamera", "build_from_args"]
