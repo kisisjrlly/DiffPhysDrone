1284fb00a0bcf9dc440901d805b10f56a6b6893a
Guodong Zhao <34215214+kisisjrlly@users.noreply.github.com>
2026-09-22 10:07:53 +0800

    feat: add grayscale sensor package

---
+++ sensors/__init__.py
@@ -0,0 +1,4 @@
+from .gray_camera_semantics import GrayCameraSemantics
+from .differentiable_gray_camera import DifferentiableGrayCamera
+
+__all__ = ["GrayCameraSemantics", "DifferentiableGrayCamera"]
